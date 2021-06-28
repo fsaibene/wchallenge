491ec0cf7713f233fc2e2bdc7972859d330c1227
Fernando Saibene <fsaibene@axoft.com>
2021-06-28 02:31:30 -0300

    fix route 2

--- src/app/pages/home/home-routing.module.ts
+++ src/app/pages/home/home-routing.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoggedGuard } from '../login/guards/logged.guard';
 import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent, canActivate: [LoggedGuard] }, ];
+const routes: Routes = [{ path: '', component: HomeComponent }, ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
